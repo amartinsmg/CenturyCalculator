b3de3f009cd121ddc5843c1088c98dbc3847f1ff
amartinsmg <andremindurimg@gmail.com>
2022-05-04 18:26:23 -0300

    webpack.config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,39 @@
-const path = require('path');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "production",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 8000,
+    open: true,
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'babel-loader',
+        test: /\.ts$/,
+        use: "babel-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".ts"],
   },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "**/*.html", context: "src/" },
+        { from: "**/*.css", context: "src/" },
+        { from: "assets/**/*" },
+      ],
+    }),
+  ],
 };
