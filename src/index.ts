136bf32a303771cc4f161450ab4716304d3fd44b
amartinsmg <andremindurimg@gmail.com>
2022-05-12 20:01:45 -0300

    Html and ts update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,70 @@
+//Convert the input number to Roman numerals
+
+function romanNumerals(num: number): string {
+  const RomanNumeralsMap: Map<number, string> = new Map([
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ]);
+  let romanNum: string = "";
+
+  if (isNaN(num) || num % 1 !== 0 || num <= 0 || num >= 4000) {
+    throw "Enter an integer greater than 0 and less than 4000";
+  }
+
+  while (num > 0) {
+    for (let [key, value] of RomanNumeralsMap) {
+      if (num >= key) {
+        romanNum += value;
+        num -= key;
+        break;
+      }
+    }
+  }
+
+  return romanNum;
+}
+
+//Convert the input number into ordinal number
+
+function ordinalNum(num: number): string {
+  const MODULE10 = num % 10,
+    MODULE100 = num % 100;
+  if (isNaN(num) || num % 1 !== 0 || num <= 0) {
+    throw "Enter a integer number greater than 0";
+  }
+  if (MODULE100 === 11 || MODULE100 === 12 || MODULE100 === 13) {
+    return `${num}th`;
+  }
+  switch (MODULE10) {
+    case 1:
+      return `${num}st`;
+    case 2:
+      return `${num}nd`;
+    case 3:
+      return `${num}rd`;
+    default:
+      return `${num}th`;
+  }
+}
+
+//Calculate the century
+
+function century(year: number): number {
+  const CENTURY = year % 100 === 0 ? year / 100 : Math.floor(year / 100) + 1;
+  return CENTURY;
+}
+
 function main(): void {
   //Constatns that store elements that will be often read or changed
 
@@ -48,73 +115,6 @@ function main(): void {
   SubmitBnt.addEventListener("click", () => {
     Form.classList.add("was-validated");
   });
-}
-
-//Convert the input number to Roman numerals
-
-function romanNumerals(num: number): string {
-  const RomanNumeralsMap: Map<number, string> = new Map([
-    [1000, "M"],
-    [900, "CM"],
-    [500, "D"],
-    [400, "CD"],
-    [100, "C"],
-    [90, "XC"],
-    [50, "L"],
-    [40, "XL"],
-    [10, "X"],
-    [9, "IX"],
-    [5, "V"],
-    [4, "IV"],
-    [1, "I"],
-  ]);
-  let romanNum: string = "";
-
-  if (isNaN(num) || num % 1 !== 0 || num <= 0 || num >= 4000) {
-    throw "Enter an integer greater than 0 and less than 4000";
-  }
-
-  while (num > 0) {
-    for (let key of RomanNumeralsMap.keys()) {
-      if (num >= key) {
-        romanNum += RomanNumeralsMap.get(key);
-        num -= key;
-        break;
-      }
-    }
-  }
-
-  return romanNum;
-}
-
-//Convert the input number into ordinal number
-
-function ordinalNum(num: number): string {
-  const MODULE10 = num % 10,
-    MODULE100 = num % 100;
-  if (isNaN(num) || num % 1 !== 0 || num <= 0) {
-    throw "Enter a integer number greater than 0";
-  }
-  if (MODULE100 === 11 || MODULE100 === 12 || MODULE100 === 13) {
-    return `${num}th`;
-  }
-  switch (MODULE10) {
-    case 1:
-      return `${num}st`;
-    case 2:
-      return `${num}nd`;
-    case 3:
-      return `${num}rd`;
-    default:
-      return `${num}th`;
-  }
-}
-
-//Calculate the century
-
-function century(year: number): number {
-  const CENTURY = year % 100 === 0 ? year / 100 : Math.floor(year / 100) + 1;
-  return CENTURY;
 }
 
 window.addEventListener("load", main);
