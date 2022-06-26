8241541bb8a0dc55ab4d4bcd5a4531f9c7eb65f0
amartinsmg <andremindurimg@gmail.com>
2022-06-26 09:56:15 -0300

    pages.js and main.css update

--- src/js/pages.js
+++ src/js/pages.js
@@ -1,75 +1,91 @@
-const EnglishPage = {
-    language: "en",
-    title: "Century Calculator",
-    navImages: [
-      {
-        link: "es/index.html",
-        src: "assets/spain-flag.png",
-        alt: "Español",
-        title: "Español",
-      },
-      {
-        link: "pt/index.html",
-        src: "assets/brazil-flag.png",
-        alt: "Português",
-        title: "Português",
-      },
-    ],
-    yearLabel: "Year",
-    yearInputMax: "",
-    yearInputPlaceholder: "Enter a number",
-    invalidFeedback: "Please, enter a integer number grater than zero.",
-    submitBtn: "Calculate",
-    centuryHeading: "Century:",
-  },
-  SpanishPage = {
-    language: "es",
-    title: "Calculadora del Siglo",
-    navImages: [
-      {
-        link: "../index.html",
-        src: "../assets/usa-flag.png",
-        alt: "English",
-        title: "English",
-      },
-      {
-        link: "../pt/index.html",
-        src: "../assets/brazil-flag.png",
-        alt: "Português",
-        title: "Português",
-      },
-    ],
-    yearLabel: "Año",
-    yearInputMax: 399900,
-    yearInputPlaceholder: "",
-    invalidFeedback:
-      "Por favor, introduzca un número mayor que cero y menor que 399900.",
-    submitBtn: "Calcular",
-    centuryHeading: "Siglo:",
-  },
-  PortuguesePage = {
-    language: "pt",
-    title: "Calculadora de Século",
-    navImages: [
-      {
-        link: "../index.html",
-        src: "../assets/usa-flag.png",
-        alt: "English",
-        title: "English",
-      },
-      {
-        link: "../es/index.html",
-        src: "../assets/spain-flag.png",
-        alt: "Español",
-        title: "Español",
-      },
-    ],
-    yearLabel: "Ano",
-    yearInputMax: 399900,
-    yearInputPlaceholder: "",
-    invalidFeedback: "Por favor, insira um número maior que zero.",
-    submitBtn: "Calcular",
-    centuryHeading: "Século:",
-  };
+function CenturyCPage(
+  language,
+  title,
+  image1,
+  image2,
+  yearLabel,
+  yearInputMax,
+  yearInputPlaceholder,
+  invalidFeedback,
+  submitBtn,
+  centuryHeading
+) {
+  this.language = language;
+  this.title = title;
+  this.navImages = [image1, image2];
+  this.yearLabel = yearLabel;
+  this.yearInputMax = yearInputMax;
+  this.yearInputPlaceholder = yearInputPlaceholder;
+  this.invalidFeedback = invalidFeedback;
+  this.submitBtn = submitBtn;
+  this.centuryHeading = centuryHeading;
+}
+
+const EnglishPage = new CenturyCPage(
+    "en",
+    "Century Calculator",
+    {
+      link: "es/index.html",
+      src: "assets/spain-flag.png",
+      alt: "Español",
+      title: "Español",
+    },
+    {
+      link: "pt/index.html",
+      src: "assets/brazil-flag.png",
+      alt: "Português",
+      title: "Português",
+    },
+    "Year",
+    null,
+    "Enter a number",
+    "Please, enter a integer number grater than zero.",
+    "Calculate",
+    "Century:"
+  ),
+  SpanishPage = new CenturyCPage(
+    "es",
+    "Calculadora del Siglo",
+    {
+      link: "../index.html",
+      src: "../assets/usa-flag.png",
+      alt: "English",
+      title: "English",
+    },
+    {
+      link: "../pt/index.html",
+      src: "../assets/brazil-flag.png",
+      alt: "Português",
+      title: "Português",
+    },
+    "Año",
+    399900,
+    "",
+    "Por favor, introduzca un número mayor que cero y menor que 399900.",
+    "Calcular",
+    "Siglo:"
+  ),
+  PortuguesePage = new CenturyCPage(
+    "pt",
+    "Calculadora de Século",
+    {
+      link: "../index.html",
+      src: "../assets/usa-flag.png",
+      alt: "English",
+      title: "English",
+    },
+    {
+      link: "../es/index.html",
+      src: "../assets/spain-flag.png",
+      alt: "Español",
+      title: "Español",
+    },
+    "Ano",
+    399900,
+    "",
+    "Por favor, insira um número maior que zero e menor que 399900",
+    "Calcular",
+    "Século:"
+  );
 
 module.exports = { EnglishPage, SpanishPage, PortuguesePage };
