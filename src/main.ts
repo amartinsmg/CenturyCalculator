952d7476868e7ef6c35ee1b83828ddfbbf33efb0
amartinsmg <andremindurimg@gmail.com>
2023-04-03 20:33:18 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import "./main.css";
 import centuryCalculator from "./ts/centurycalculator";
 
+/**
+  This statement calls the main function in centurycalculator.ts when the web page is fully
+  loaded. It ensures that all necessary resources and elements are available before the
+  calculator is initialized, preventing errors that could occur if the calculator were
+  initialized too early
+ */
+
 window.addEventListener("load", () =>
   centuryCalculator(
     "#input-form",
