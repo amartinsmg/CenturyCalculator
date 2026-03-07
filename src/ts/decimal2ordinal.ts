19ab8abf659f524d9df937bf958120b71ecd33ea
André Martins <andremindurimg@gmail.com>
2026-03-07 11:27:49 -0300

    Update decimal2ordinal.ts to imporove readability

--- src/ts/decimal2ordinal.ts
+++ src/ts/decimal2ordinal.ts
@@ -6,12 +6,12 @@
  */
 
 function decimal2ordinal(num: number): string {
-  const MODULUS_10 = num % 10,
-    MODULUS_100 = num % 100;
+  const mod10 = num % 10,
+    mod100 = num % 100;
   if (isNaN(num) || num % 1 || num <= 0)
     throw "Enter a integer number greater than 0";
-  if ([11, 12, 13].includes(MODULUS_100)) return `${num}th`;
-  switch (MODULUS_10) {
+  if ([11, 12, 13].includes(mod100)) return `${num}th`;
+  switch (mod10) {
     case 1:
       return `${num}st`;
     case 2:
