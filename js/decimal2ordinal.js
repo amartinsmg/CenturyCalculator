aadeccbe0d2a27181935ca6a894262b13700e259
amartinsmg <amartinsmg@users.noreply.github.com>
2026-04-12 16:28:07 +0000

    deploy: fb98630a0e925a3a3b66370cf9ed1a3c017b8bbe

---
+++ js/decimal2ordinal.js
@@ -0,0 +1,26 @@
+/**
+  Converts a decimal number to its corresponding ordinal representation.
+    @param num - The decimal number to be converted.
+    @returns - The ordinal representation of the input number as a string.
+    @throws - If the input is not a positive integer.
+ */
+
+function decimal2ordinal(num) {
+  const mod10 = num % 10,
+    mod100 = num % 100;
+  if (isNaN(num) || num % 1 || num <= 0)
+    throw "Enter a integer number greater than 0";
+  if ([11, 12, 13].includes(mod100)) return `${num}th`;
+  switch (mod10) {
+    case 1:
+      return `${num}st`;
+    case 2:
+      return `${num}nd`;
+    case 3:
+      return `${num}rd`;
+    default:
+      return `${num}th`;
+  }
+}
+
+export default decimal2ordinal;
---
+++ js/decimal2roman.js
@@ -0,0 +1,42 @@
+/**
+  This function uses a map that relates decimal numbers to their respective Roman numerals.
+    @param num - The decimal number to be converted to Roman numeral.
+    @return - A string representing the Roman numeral.
+    @throws - An error message if the input is not a positive integer or greater than 4000.
+ */
+
+function decimal2roman(num) {
+  const romanNumeralsMap = new Map([
+    [1000, "M"],
+    [900, "CM"],
+    [500, "D"],
+    [400, "CD"],
+    [100, "C"],
+    [90, "XC"],
+    [50, "L"],
+    [40, "XL"],
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"],
+  ]);
+  let romanNum = "";
+
+  if (isNaN(num) || num % 1 || num <= 0 || num >= 4000)
+    throw "Enter an integer greater than 0 and less than 4000";
+
+  while (num > 0) {
+    for (let [key, value] of romanNumeralsMap) {
+      if (num >= key) {
+        romanNum += value;
+        num -= key;
+        break;
+      }
+    }
+  }
+
+  return romanNum;
+}
+
+export default decimal2roman;
